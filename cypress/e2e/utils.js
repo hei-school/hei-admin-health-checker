240668912a3d0f01f9215d460387214569c2b2f7
hei-admin-devs <hei-admin-dev@mail.hei.school>
2026-03-05 13:49:12 +0300

    fix: update test utils

--- cypress/e2e/utils.js
+++ cypress/e2e/utils.js
@@ -1,22 +1,22 @@
 export const credentialsMap = {
   STUDENT: {
-    email: "test+manitra@mail.hei.school",
+    email: "test+student@mail.hei.school",
     password: Cypress.env("CYPRESS_STUDENT1_PASSWORD"),
   },
   TEACHER: {
-    email: "test+teacher1@hei.school",
+    email: "test+teacher1@mail.hei.school",
     password: Cypress.env("CYPRESS_TEACHER1_PASSWORD"),
   },
   MANAGER: {
-    email: "test+manager1@hei.school",
+    email: "test+manager1@mail.hei.school",
     password: Cypress.env("CYPRESS_MANAGER1_PASSWORD"),
   },
   ADMIN: {
-    email: "test+admin@hei.school",
+    email: "test+admin@mail.hei.school",
     password: Cypress.env("CYPRESS_ADMIN1_PASSWORD"),
   },
   MONITOR: {
-    email: "test+monitor@hei.school",
+    email: "test+monitor@mail.hei.school",
     password: Cypress.env("CYPRESS_MONITOR1_PASSWORD"),
   },
 };
